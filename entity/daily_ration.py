8d0f890618402b443295f8d79c51284cbc969c8c
Mark_Tarulin <evilsobol01@gmail.com>
2023-12-21 21:14:18 +0700

    Реализован метод, который выводит объединенный список ингредиентов для дневного рациона

--- entity/daily_ration.py
+++ entity/daily_ration.py
@@ -18,3 +18,19 @@ class DailyRation:
         self.lunch.get_random_dish('Основные блюда')
         self.dinner.get_random_dish('Основные блюда')
         self.snack.get_random_dish('Салаты')
+
+    def get_union_ingredients(self):
+        ingredients_dicts = [
+            self.breakfast.ingredients,
+            self.lunch.ingredients,
+            self.dinner.ingredients,
+            self.snack.ingredients
+        ]
+        union_ingredients_dicts = dict()
+        for ingredients_dict in ingredients_dicts:
+            for key, value in ingredients_dict.items():
+                if key in union_ingredients_dicts:
+                    union_ingredients_dicts[key] = union_ingredients_dicts[key] + " и " + value
+                else:
+                    union_ingredients_dicts[key] = value
+        return union_ingredients_dicts
